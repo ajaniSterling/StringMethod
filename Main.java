ac5e497c333daf15e738cf0d05b3619bb945036b
ajaniSterling <ajani.sterling@hgs.hiddengeniusproject.org>
2021-12-11 02:30:28 +0000

    Developed 2.4 Using String Method

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ *  This program demonstrates
+ *  length String methods.
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+      String message;    // To hold a string
+      int n;            // To hold length of String
+
+      message = "The Java Tutorials";
+      n = message.length();
+      System.out.println("The length of message : " + n);
+   }
+}
